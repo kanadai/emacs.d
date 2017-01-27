b4c2e564e81cfa9068267d5348235ccab5ef9ac7
Yutaka Kanai <kanai@iaxis.co.jp>
2017-01-27 23:01:22 +0900

    jump to definition

--- init-loader/00_keybindings.el
+++ init-loader/00_keybindings.el
@@ -7,7 +7,7 @@
 (global-set-key "\C-h" 'backward-delete-char)
 (define-key global-map [?¥] [?\\])
 (define-key global-map (kbd "C-z") nil)
-(define-key global-map [f12] 'dumb-jump-go)
+(define-key global-map (kbd "C-]") 'dumb-jump-go)
 (define-key global-map (kbd "M-<up>") 'beginning-of-buffer)
 (define-key global-map (kbd "M-<down>") 'end-of-buffer)
 ;; (setq evil-insert-state-map nil)
